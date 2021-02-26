26ffdf7db4109e4d5385827017f7ea94b5427797
ITmxs <17752170152@163.com>
2021-02-26 09:19:30 +0800

    instal

---
+++ lib/constant/post_json.dart
@@ -0,0 +1,56 @@
+List posts = [
+  {
+    "id": 1,
+    "name": "panhchaneath_ung",
+    "profileImg":
+        "https://pic2.zhimg.com/v2-7eacbed472d922c0934261e6113916fc_1440w.jpg?source=172ae18b",
+    "postImg":
+        "https://pic2.zhimg.com/v2-7eacbed472d922c0934261e6113916fc_1440w.jpg?source=172ae18b",
+    "caption": " SERIOUS MODE during SERIOUS RAINY DAY",
+    "isLoved": true,
+    "commentCount": "10",
+    "likedBy": "whereavygoes",
+    "timeAgo": "1 day ago"
+  },
+  {
+    "id": 2,
+    "name": "whereavygoes",
+    "profileImg":
+        "https://pic2.zhimg.com/v2-7eacbed472d922c0934261e6113916fc_1440w.jpg?source=172ae18b",
+    "postImg":
+        "https://pic2.zhimg.com/v2-7eacbed472d922c0934261e6113916fc_1440w.jpg?source=172ae18b",
+    "caption":
+        " ទឹកមុខនៅពេលយើងអត់ចង់ថតរូបនៅកន្លែងសួនផ្កា ហើយគេបង្ខំអោយយើងថត។ 😏😒",
+    "isLoved": true,
+    "commentCount": "10",
+    "likedBy": "sonitakhoun",
+    "timeAgo": "1 day ago"
+  },
+  {
+    "id": 3,
+    "name": "allef_vinicius",
+    "profileImg":
+        "https://pic2.zhimg.com/v2-7eacbed472d922c0934261e6113916fc_1440w.jpg?source=172ae18b",
+    "postImg":
+        "https://pic2.zhimg.com/v2-7eacbed472d922c0934261e6113916fc_1440w.jpg?source=172ae18b",
+    "caption": " It is a good to day",
+    "isLoved": false,
+    "commentCount": "60",
+    "likedBy": "babysweetiepie",
+    "timeAgo": "3 day ago"
+  },
+  {
+    "id": 4,
+    "name": "babysweetiepie",
+    "profileImg":
+        "https://pic2.zhimg.com/v2-7eacbed472d922c0934261e6113916fc_1440w.jpg?source=172ae18b",
+    "postImg":
+        "https://pic2.zhimg.com/v2-7eacbed472d922c0934261e6113916fc_1440w.jpg?source=172ae18b",
+    "caption":
+        "I love how a 6-year-old kid took better photos than many of my friends out there 😂",
+    "isLoved": false,
+    "commentCount": "70",
+    "likedBy": "sonitakhoun",
+    "timeAgo": "3 day ago"
+  }
+];
---
+++ lib/constant/story_json.dart
@@ -0,0 +1,38 @@
+// user prfile
+String profile =
+    "https://pic2.zhimg.com/v2-7eacbed472d922c0934261e6113916fc_1440w.jpg?source=172ae18b";
+String name = "panhchaneath_ung";
+
+// stories
+List stories = [
+  {
+    "id": 1,
+    "img":
+        "https://pic2.zhimg.com/v2-7eacbed472d922c0934261e6113916fc_1440w.jpg?source=172ae18b",
+    "name": "sonitakhoun"
+  },
+  {
+    "id": 2,
+    "img":
+        "https://pic2.zhimg.com/v2-7eacbed472d922c0934261e6113916fc_1440w.jpg?source=172ae18b",
+    "name": "babysweetiepie"
+  },
+  {
+    "id": 3,
+    "img":
+        "https://pic2.zhimg.com/v2-7eacbed472d922c0934261e6113916fc_1440w.jpg?source=172ae18b",
+    "name": "whereavygoes"
+  },
+  {
+    "id": 4,
+    "img":
+        "https://pic2.zhimg.com/v2-7eacbed472d922c0934261e6113916fc_1440w.jpg?source=172ae18b",
+    "name": "collins_lesulie"
+  },
+  {
+    "id": 5,
+    "img":
+        "https://pic2.zhimg.com/v2-7eacbed472d922c0934261e6113916fc_1440w.jpg?source=172ae18b",
+    "name": "tyler_nix"
+  }
+];
